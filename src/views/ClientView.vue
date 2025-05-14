daaa9a329631ee744c59000bb11d3a6be7deb59e
Omar Nashawati <oceanprime.on@gmail.com>
2025-05-14 12:03:42 +0300

    sidbar UI

--- src/views/ClientView.vue
+++ src/views/ClientView.vue
@@ -1,11 +1,53 @@
 <script setup>
+
+  const collections = [
+    "Nature & Landscapes",
+    "Urban & City Life",
+    "Abstract Backgrounds",
+    "Animals & Wildlife",
+    "Technology & Innovation",
+  ]
 </script>
 
 <template>
-  Client
+  <div class="client-view-wrapper">
+
+    <div class="sidebar-wrapper">
+      <h2>Collections</h2>
+      <nav>
+        <ul>
+          <li v-for="item in collections">{{ item }}</li>
+        </ul>
+      </nav>
+    </div>
+
+    <div class="gallery-wrapper"></div>
+
+
+  </div>
 </template>
 
 <style scoped>
+  .sidebar-wrapper{
+    background-color: var(--background-alt);
+    width: 250px;
+    height: 100%;
+    padding: 1rem 0rem;
+  }
 
+  .sidebar-wrapper>h2{
+    padding: 1rem;
+  }
+
+  li {
+    color: var(--text-muted);
+    padding: 8px 1rem;
+  }
+
+  li:hover {
+    background-color: var(--btn-primary-hover);
+    color: var(--text);
+    cursor: pointer;
+  }
   
 </style>
