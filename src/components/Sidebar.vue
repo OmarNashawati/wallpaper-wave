04dc3779052f69600ef243708e574a4e042915e3
Omar Nashawati <oceanprime.on@gmail.com>
2025-05-14 12:06:44 +0300

    move sidebar to component

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,46 @@
+<script setup>
+  const collections = [
+    "Nature & Landscapes",
+    "Urban & City Life",
+    "Abstract Backgrounds",
+    "Animals & Wildlife",
+    "Technology & Innovation",
+  ]
+</script>
+
+<template>
+
+ <div class="sidebar-wrapper">
+    <h2>Collections</h2>
+    <nav>
+      <ul>
+        <li v-for="item in collections">{{ item }}</li>
+      </ul>
+    </nav>
+  </div>
+
+</template>
+
+<style scoped>
+  .sidebar-wrapper{
+    background-color: var(--background-alt);
+    width: 250px;
+    height: 100%;
+    padding: 1rem 0rem;
+  }
+
+  .sidebar-wrapper>h2{
+    padding: 1rem;
+  }
+
+  li {
+    color: var(--text-muted);
+    padding: 8px 1rem;
+  }
+
+  li:hover {
+    background-color: var(--btn-primary-hover);
+    color: var(--text);
+    cursor: pointer;
+  }
+</style>
--- src/views/ClientView.vue
+++ src/views/ClientView.vue
@@ -1,25 +1,12 @@
 <script setup>
+  import Sidebar from '@/components/Sidebar.vue';
 
-  const collections = [
-    "Nature & Landscapes",
-    "Urban & City Life",
-    "Abstract Backgrounds",
-    "Animals & Wildlife",
-    "Technology & Innovation",
-  ]
 </script>
 
 <template>
   <div class="client-view-wrapper">
 
-    <div class="sidebar-wrapper">
-      <h2>Collections</h2>
-      <nav>
-        <ul>
-          <li v-for="item in collections">{{ item }}</li>
-        </ul>
-      </nav>
-    </div>
+    <Sidebar />
 
     <div class="gallery-wrapper"></div>
 
@@ -28,26 +15,5 @@
 </template>
 
 <style scoped>
-  .sidebar-wrapper{
-    background-color: var(--background-alt);
-    width: 250px;
-    height: 100%;
-    padding: 1rem 0rem;
-  }
-
-  .sidebar-wrapper>h2{
-    padding: 1rem;
-  }
-
-  li {
-    color: var(--text-muted);
-    padding: 8px 1rem;
-  }
-
-  li:hover {
-    background-color: var(--btn-primary-hover);
-    color: var(--text);
-    cursor: pointer;
-  }
   
 </style>
